3e67be9cbae4cf48ff0c723175dc764fbd3d0b6f
Michael Davids <michaelcdavids@gmail.com>
2020-05-19 11:12:04 +0200

    added product and subclasses + overloaded buy method in OverloadedVendingMachine

--- src/main/java/vending/OverloadedVendingMachine.java
+++ src/main/java/vending/OverloadedVendingMachine.java
@@ -1,4 +1,33 @@
 package vending;
 
+import vending.product.Chocolate;
+import vending.product.Product;
+import vending.product.SaltySnack;
+import vending.product.SoftDrink;
+
 public class OverloadedVendingMachine {
+    SoftDrink softDrink;
+    SaltySnack saltySnack;
+    Chocolate chocolate;
+
+    OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty){
+        // set the stockLevel instance variables for each product in the constructor
+    }
+
+    public void buy(SoftDrink softDrink){
+
+    }
+
+    public void buy(SaltySnack saltySnack){
+
+    }
+
+    public void buy(Chocolate chocolate){
+
+    }
+
+    public void buy(Product product){
+
+    }
+
 }
