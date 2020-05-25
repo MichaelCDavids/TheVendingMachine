46281b516e98b124b520ddc423bd3bea0b2be67e
Michael Davids <michaelcdavids@gmail.com>
2020-05-25 15:49:41 +0200

    super class for vending machines

--- src/main/java/vending/VendingMachine.java
+++ src/main/java/vending/VendingMachine.java
@@ -1,4 +1,11 @@
 package vending;
 
-public class VendingMiachine {
+import vending.exceptions.ProductNotFoundException;
+import vending.product.Product;
+
+public abstract class VendingMachine {
+    public abstract void buy( Product p, int i ) throws ProductNotFoundException;
+    public abstract int getStock(Product p);
+    public abstract void addStock(Product p, int i);
+    public abstract void buy( Product product );
 }
